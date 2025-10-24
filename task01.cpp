ffed0f93242a2895f6ccadd556eb08dbf689918c
f2025-0798-tech <f2025-0798@bnu.edu.pk>
2025-10-24 15:27:31 +0500

    Create task01.cpp

---
+++ task01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+ string name = "Rahiba khan ";
+ string dept = "SCIT";
+ string quote = "\"no.\" - rahiba";
+
+ cout << "*******************************\n";
+ cout << "*\tName:\t\t" << name << "\n";
+ cout << "*\tDepartment:\t" << dept << "\n";
+ cout << "*\tQuote:\t\t" << quote << "\n";
+ cout << "*******************************\n";
+
+ return 0;
+}
